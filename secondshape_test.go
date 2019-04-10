524b8801ead18917b318342012474b8b7c1b7828
Andres Rios <Andres_Rios@epam.com>
2019-04-10 16:58:06 +0300

    Test a positive case, just in case

--- secondshape_test.go
+++ secondshape_test.go
@@ -44,7 +44,6 @@ func TestTryFnStrategyMaxAttempt(t *testing.T) {
 }
 
 func TestTryFnStrategyMaxAttemptWithDelay(t *testing.T) {
-
 	currentAttempt := 0
 	errExpected := errors.New("expected error")
 
@@ -73,6 +72,43 @@ func TestTryFnStrategyMaxAttemptWithDelay(t *testing.T) {
 		t.Error("strategy must hold an error")
 	}
 	if strategy.MaxAttempts != currentAttempt {
+		t.Error("currentAttempt differs from MaxAttempts")
+	}
+}
+
+func TestTryFnStrategyMaxAttemptWithDelayBelowMaxAttempts(t *testing.T) {
+	currentAttempt := 0
+	StopAttempt := 3
+	errExpected := errors.New("expected error")
+
+	startTime := time.Now()
+
+	strategy := Retry{
+		MaxAttempts: 5,
+		Delay:       time.Millisecond * 100,
+	}
+
+	errActual := TryWithStrategy(func() error {
+		currentAttempt++
+		if currentAttempt == StopAttempt {
+			return nil
+		}
+		return errExpected
+	}, &strategy)
+
+	endTime := time.Now()
+
+	if endTime.Sub(startTime) > time.Millisecond*4*100 {
+		t.Error("Incorrect delay")
+	}
+
+	if errActual != nil {
+		t.Error("fn returns an unexpected error")
+	}
+	if strategy.Error != nil {
+		t.Error("strategy must hold an error")
+	}
+	if StopAttempt != currentAttempt {
 		t.Error("currentAttempt differs from MaxAttempts")
 	}
 }
